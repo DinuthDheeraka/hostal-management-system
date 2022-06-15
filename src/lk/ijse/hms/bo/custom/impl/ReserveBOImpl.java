36300c00c68fb8b2f7f28db4fe2c868f8d23d710
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-06-16 00:00:58 +0530

    Added BO & Impl classes(Reserve,Student,Room)

---
+++ src/lk/ijse/hms/bo/custom/impl/ReserveBOImpl.java
@@ -0,0 +1,11 @@
+/**
+ * @author : Dinuth Dheeraka
+ * Project Name: hostal-management-system
+ * Created     : 6/15/2022 11:58 PM
+ */
+package lk.ijse.hms.bo.custom.impl;
+
+import lk.ijse.hms.bo.custom.ReserveBO;
+
+public class ReserveBOImpl implements ReserveBO {
+}
