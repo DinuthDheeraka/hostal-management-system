0e37a7e6f3c562d6e2e8bc80b6a4c500eda014ff
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-06-15 19:15:55 +0530

    Added hostal-management-system

---
+++ src/lk/ijse/hms/util/FactoryConfiguration.java
@@ -0,0 +1,11 @@
+/**
+ * @author : Dinuth Dheeraka
+ * Project Name: hostal-management-system
+ * Created     : 6/15/2022 7:12 PM
+ */
+package lk.ijse.hms.util;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    
+}
