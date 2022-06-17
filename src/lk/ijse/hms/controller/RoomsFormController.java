dd6d103c2eeb99b2bbf3aaf849e84f1a3ab28bc9
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-06-17 23:25:39 +0530

    Created Student-Form Design

---
+++ src/lk/ijse/hms/controller/RoomsFormController.java
@@ -0,0 +1,7 @@
+/**
+ * @author     : Dinuth Dheeraka
+ * Project Name: hostal-management-system
+ * Created     : 6/17/2022 11:25 PM
+ */
+package lk.ijse.hms.controller;public class RoomsFormController {
+}
---
+++ src/lk/ijse/hms/controller/StudentsFormController.java
@@ -0,0 +1,7 @@
+/**
+ * @author     : Dinuth Dheeraka
+ * Project Name: hostal-management-system
+ * Created     : 6/17/2022 5:58 PM
+ */
+package lk.ijse.hms.controller;public class StudentsFormController {
+}
