ac7b9c38fa0129deffcf05c647dbc502da24dee1
DinuthDheeraka <dinuthdheeraka345@gmail.com>
2022-06-19 02:32:22 +0530

    Added getAllRooms and set data to Room table

---
+++ src/lk/ijse/hms/dto/RoomDTO.java
@@ -0,0 +1,7 @@
+/**
+ * @author     : Dinuth Dheeraka
+ * Project Name: hostal-management-system
+ * Created     : 6/19/2022 2:05 AM
+ */
+package lk.ijse.hms.dto;public class RoomDTO {
+}
---
+++ src/lk/ijse/hms/view/tdm/RoomTM.java
@@ -0,0 +1,7 @@
+/**
+ * @author     : Dinuth Dheeraka
+ * Project Name: hostal-management-system
+ * Created     : 6/19/2022 2:14 AM
+ */
+package lk.ijse.hms.view.tdm;public class RoomTM {
+}
